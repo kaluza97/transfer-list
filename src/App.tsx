1d12db81a88502fadfa28e9b92ade80c0fc2969b
Piotr Kaluzny <piotrkaluzny@MacBook-Air-Piotr.local>
2022-06-25 23:16:32 +0200

    add styled components and initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import { Header } from 'components/header/Header';
 import './App.css';
 
-function App() {
-    return <div className="App"></div>;
-}
-
-export default App;
+export const App = () => {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+};
