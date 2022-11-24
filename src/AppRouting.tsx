3d2a433caea1ada26e848eb653a7916e2df8f917
kaluza97 <kaluzny.piotr1997@gmail.com>
2022-11-24 22:08:25 +0100

    change logout route

--- src/AppRouting.tsx
+++ src/AppRouting.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DashboardPage } from 'components/dashboardPage/Component';
 import { Login } from 'components/auth/Login';
+import { Logout } from 'components/auth/Logout';
 import { ProtectedRoutes } from 'components/routes/ProtectedRoutes';
 import { DASHBOARD_PAGE, MAIN } from 'constants/Routes';
 import { useAppSelector } from 'hooks/useAppSelector';
@@ -13,6 +14,7 @@ export const AppRouting = () => {
       <p>Your id: {id}</p>
       <Routes>
         <Route path={MAIN} element={<Login />} />
+        <Route path={MAIN} element={<Logout />} />
         <Route path={DASHBOARD_PAGE} element={<ProtectedRoutes route={<DashboardPage />} />} />
       </Routes>
     </BrowserRouter>
--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,4 @@
 export const REGISTER = '/register';
-export const LOGIN = '/login';
 export const MAIN = '/';
 export const DASHBOARD_PAGE = '/dashboard_page';
 export const ACCOUNT = '/account';
